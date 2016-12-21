cca36bf39dcdc7be9c2dd768a441f48b0bd8baa9
Yaakov Yosef Miller <ykv00001@gmail.com>
2016-12-21 11:46:16 -0500

    Working functions A2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Function porduces results of for the Programming assigment 2
 
-## Write a short comment describing this function
+## This function caches the inverse of a matrix, it produces a list of special values to 
+## capture the cache in the other function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        s <- NULL
+        set <- function(y) {
+                x <<- y
+                s <<- NULL
+        }
+        get <- function() x
+        setsolve <- function(solve) s <<- solve
+        getsolve <- function() s
+        list(set = set, get = get,
+             setsolve = setsolve,
+             getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+
+## calculates the inverse of the matrix entered in the previous function
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        s <-x$getsolve()
+        if(!is.null(s)) {
+                message("getting cached data")
+                return(s)
+        }
+        data <- x$get()
+        s <- solve(data,...)
+        x$setsolve(s)
+        s
 }
